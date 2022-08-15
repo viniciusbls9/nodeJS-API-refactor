7534a4e878b2f254218e3bdb70d326a27f3321db
Vinicius Benedito <beneditoloura@gmail.com>
2022-08-15 07:27:48 -0300

    ♻️ refactor: change like router return

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
