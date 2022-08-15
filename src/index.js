9c1c100561877997f3807b8e497d77c742a9c573
Vinicius Benedito <beneditoloura@gmail.com>
2022-08-15 07:42:58 -0300

    ♻️ refactor: return likes 0 in repositories put router

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ app.put("/repositories/:id", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const repository = { ...repositories[repositoryIndex], ...updatedRepository, likes: 0 };
 
   repositories[repositoryIndex] = repository;
 
